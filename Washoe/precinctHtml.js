f6891a7f8e592fe1cfc99f2312511c28239b9e6c
marcus <maromboy@gmail.com>
2021-11-07 16:35:50 -0500

    legend, map controls

--- Washoe/precinctHtml.js
+++ Washoe/precinctHtml.js
@@ -1,5 +1,7 @@
 function get_precinct_html(e){
-    var html = '<div style="width: 150px;"><div style="text-align: center; background-color:grey; color:white;"><strong>'+e.features[0].properties.PRECINCT + '</strong></div>'
+    var html = '<div style="width: 150px;">'
+    html = html+ '<div style="text-align: center; background-color:grey; color:white;">'
+    html = html + '<strong> Precinct - '+e.features[0].properties.PRECINCT + '</strong></div>'
     html = html + '<br> Township: ' + e.features[0].properties.TOWNSHIP 
     html = html + '<br> Committee Capitan: '  + e.features[0].properties.CAPITAN 
     html = html + '<br> Seats Filled: '  + e.features[0].properties.FILLED 
