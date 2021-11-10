7f06de7eef0a48a8bcc8fe4087b1a23f4c220e2a
marcus <maromboy@gmail.com>
2021-11-09 22:25:27 -0500

    cleaned html string builder

--- Washoe/precinctHtml.js
+++ Washoe/precinctHtml.js
@@ -1,12 +1,11 @@
 function get_precinct_html(e){
     var html = '<div style="width: 150px;">'
-    html = html+ '<div style="text-align: center; background-color:grey; color:white;">'
-    html = html + '<strong> Precinct - '+e.features[0].properties.PRECINCT + '</strong></div>'
-    html = html + '<br> Township: ' + e.features[0].properties.TOWNSHIP 
-    html = html + '<br> Committee Capitan: '  + e.features[0].properties.CAPITAN 
-    html = html + '<br> Seats Filled: '  + e.features[0].properties.FILLED 
-    html = html + '<br> Seats Vacant: '  + e.features[0].properties.VACANT 
-    
-    html = html + '</div> '
+    html += '<div style="text-align: center; background-color:grey; color:white;">'
+    html += '<strong> Precinct - '+e.features[0].properties.PRECINCT + '</strong></div>'
+    html += '<br> Township: ' + e.features[0].properties.TOWNSHIP 
+    html += '<br> Committee Capitan: '  + e.features[0].properties.CAPITAN 
+    html += '<br> Seats Filled: '  + e.features[0].properties.FILLED 
+    html += '<br> Seats Vacant: '  + e.features[0].properties.VACANT 
+    html += '</div> '
     return html
 };
