064b436133250469c28a9852c90d6b2a39fec02d
marcus <maromboy@gmail.com>
2021-12-07 06:35:10 -0500

    updated popup

--- Washoe/precinctHtml.js
+++ Washoe/precinctHtml.js
@@ -1,11 +1,20 @@
-function get_precinct_html(e){
+function get_precinct_html(properties){
     var html = '<div style="width: 150px;">'
     html += '<div style="text-align: center; background-color:grey; color:white;">'
-    html += '<strong> Precinct - '+e.features[0].properties.PRECINCT + '</strong></div>'
-    html += '<br> Township: ' + e.features[0].properties.TOWNSHIP 
-    html += '<br> Committee Capitan: '  + e.features[0].properties.CAPITAN 
-    html += '<br> Seats Filled: '  + e.features[0].properties.FILLED 
-    html += '<br> Seats Vacant: '  + e.features[0].properties.VACANT 
+    html += '<strong> Precinct - ' + properties.PRECINCT + '</strong></div>'
+    html += 'Township: ' + properties.TOWNSHIP 
+    html += '<br> Committee Capitan: '  + properties.CAPITAN 
+    html += '<br> Seats Filled: '  + properties.FILLED 
+    html += '<br> Seats Vacant: '  + properties.VACANT 
     html += '</div> '
+    return html
+};
+
+function get_district_html(properties){
+    var html = '<div style="width: 150px;">'
+    html += '<div style="text-align: center; background-color:purple; color:white;">'
+    html += '<strong>' +  properties.DISTRICT + '</strong></div>'
+    html +=  'Supervisor: ' + properties.ASSEMBLYPE + '</div>'
+
     return html
 };
