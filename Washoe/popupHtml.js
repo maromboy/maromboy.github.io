49f6e77ddd65669f483f46425614aeae140e639a
SUNRISE-ENG\mromboy <mromboy@sunrise-eng.com>
2021-12-08 10:35:58 -0500

    fixed typo

--- Washoe/popupHtml.js
+++ Washoe/popupHtml.js
@@ -2,7 +2,7 @@ function get_precinct_html(properties){
     var html = '<div style="width: 150px;">'
     html += '<div style="text-align: center; background-color:grey; color:white;">'
     html += '<strong> Precinct - ' + properties.PRECINCT + '</strong></div>'
-    html += 'Committee Capitan: '  + 'TBD'
+    html += 'Committee Captain: '  + 'TBD'
     html += '<br> Phone: '  + '775-111-8888'
     html += '<br> <a href="mailto:someone@example.com">Send email</a>'
     html += '</div><br> '
